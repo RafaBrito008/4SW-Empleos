b0abd40f61a4d303e53873b51a3b1f087e37074a
Sebas <dpalate2646@uta.edu.ec>
2022-01-15 00:04:37 -0500

    Creación de clases y carpetas.

---
+++ GestionEmpleos/src/gestores/GestorEmpleos.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestores;
+
+/**
+ *
+ * @author sebas
+ */
+public class GestorEmpleos {
+
+    public boolean mostrarEmpleos() {
+        /*
+        Método para mostrar los Empleos de la Base de Datos
+         */
+        return false;
+    }
+
+    public boolean insertarEmpleo() {
+        /*
+        Método para insertar un Empleo
+         */
+        return false;
+    }
+
+    public boolean elimimanrEmpleo() {
+        return false;
+    }
+
+    public boolean modificarEmpleo() {
+        return false;
+    }
+
+}
---
+++ GestionEmpleos/src/gestores/GestorOfertantes.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestores;
+
+/**
+ *
+ * @author sebas
+ */
+public class GestorOfertantes {
+
+    public boolean mostrarOfertantes() {
+        /*
+        Método para obtener los ofertantes de la base de datos
+         */
+        return false;
+    }
+
+    public boolean insertarOfertantes() {
+        /*
+        Método para insertar ofertantes y cargarlos en la Base de Datos
+         */
+        return false;
+    }
+
+    public boolean elimianrOfertantes() {
+        /*
+        Método para la eliminacion de un ofertante desde la Base de Datos
+         */
+        return false;
+    }
+
+    public boolean modificarOfertante() {
+        /*
+        Método para mod
+         */
+        return false;
+    }
+
+    public boolean seleccionarOfertante() {
+        /*
+        Método en desición, ya que con acciones se puede seleccionar a un ofertantes
+         */
+        return false;
+    }
+
+}
