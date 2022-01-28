d6b40ff6cece6a571bba01dcace9810dcd7cd5fb
Rafael <rbrito9162@uta.edu.ec>
2022-01-28 09:49:44 -0500

    Ingreso por tipo de usuario en Login

--- GestionEmpleos/src/Interfaces/Login.java
+++ GestionEmpleos/src/Interfaces/Login.java
@@ -18,7 +18,9 @@ import sql.ConexionSQL;
  * @author Rafa Brito
  */
 public class Login extends javax.swing.JFrame {
+
     String ced_login;
+
     /**
      * Creates new form login
      */
@@ -35,21 +37,38 @@ public class Login extends javax.swing.JFrame {
             String username = jtxtId.getText();
             String password = jtxtPassword.getText();
 
-            String sql = "SELECT CED_USU, USERNAME_USU, (aes_decrypt(PASSWORD_USU, 'AES')) FROM usuarios "
+            String sqlOfer = "SELECT CED_USU, USERNAME_USU, (aes_decrypt(PASSWORD_USU, 'AES')), TIPO_USU FROM usuarios "
                     + "WHERE USERNAME_USU = '" + username + "' "
-                    + "AND (aes_decrypt(PASSWORD_USU, 'AES')) = '" + password + "'";
+                    + "AND (aes_decrypt(PASSWORD_USU, 'AES')) = '" + password + "' "
+                    + "AND TIPO_USU = 'OFERTANTE'";
 
+            String sqlCli = "SELECT CED_USU, USERNAME_USU, (aes_decrypt(PASSWORD_USU, 'AES')), TIPO_USU FROM usuarios "
+                    + "WHERE USERNAME_USU = '" + username + "' "
+                    + "AND (aes_decrypt(PASSWORD_USU, 'AES')) = '" + password + "' "
+                    + "AND TIPO_USU = 'CLIENTE'";
 
-            Statement st = cn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            while (rs.next()) {
-                ced_login=rs.getString("CED_USU");
+            Statement stOfer = cn.createStatement();
+            ResultSet rsOfer = stOfer.executeQuery(sqlOfer);
+
+            Statement stCli = cn.createStatement();
+            ResultSet rsCli = stCli.executeQuery(sqlCli);
+
+            if (rsOfer.next()) {
+                ingreso = true;
+                JOptionPane.showMessageDialog(rootPane, "Ingreso exitoso.");
+                this.dispose();
+                while (rsOfer.next()) {
+                    ced_login = rsOfer.getString("CED_USU");
+                }
             }
 
-            if (rs.next()) {
+            if (rsCli.next()) {
                 ingreso = true;
-                JOptionPane.showMessageDialog(rootPane, "Ingreso exitoso");
+                JOptionPane.showMessageDialog(rootPane, "Ingreso exitoso.");
                 this.dispose();
+                while (rsOfer.next()) {
+                    ced_login = rsOfer.getString("CED_USU");
+                }
             }
 
         } catch (SQLException ex) {
