6496b19f1c191d42a1051098bf96a734e1ec69e6
Rafael <rbrito9162@uta.edu.ec>
2022-01-30 19:54:14 -0500

    Proyecto funcional.

---
+++ GestionEmpleos/src/sql/ConexionSQL.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author sebas
+ */
+public class ConexionSQL {
+
+    Connection conectar;
+
+    public Connection conectar() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //Obtiene la direccion de la base de datos
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost/SistemaEmpleos", "root", "");
+            //conectar = DriverManager.getConnection("jdbc:mysql://localhost/gestionempleos", "root", "");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error " + ex);
+        }
+        return conectar;
+    }
+
+}
